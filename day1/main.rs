04a04a8732d0083c0393ae15253fa27873cf35b7
Charles Czysz <charles@czysz.io>
2019-12-02 19:21:29 -0600

    Day1 init

---
+++ day1/main.rs
@@ -0,0 +1,16 @@
+use std::env;
+use std::fs;
+
+fn convert_mass(mass: f32) -> f32 {
+    let fuel: f32 = (mass / 3.0).floor() - 2.0;
+    return fuel;
+}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file = &args[1];
+    // let mass = &args[1];
+    // let mass: f32 = mass.trim().parse::<f32>().expect("Not a number.");
+    // args() returns an iterator. Collect() turns into vector.
+    // let fuel: f32 = convert_mass(mass);
+    // println!("{}", fuel);
+}
